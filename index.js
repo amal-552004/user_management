379f1913e3b01f4f32ea6273f2b443d8bd58e941
amal ch <159940996+amal-552004@users.noreply.github.com>
2025-02-07 17:55:55 +0100

    servir les fichiers statiques depuis le dossier public

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,15 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Servir les fichiers statiques depuis le dossier "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route principale pour afficher index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Connexion à la base de données SQLite
 const db = new sqlite3.Database("./users.db", (err) => {
   if (err) {
     console.error("Erreur de connexion à la base de données", err);
@@ -20,36 +30,29 @@ const db = new sqlite3.Database("./users.db", (err) => {
   }
 });
 
-// Route de test
-app.get("/", (req, res) => {
-  res.send("API User Management is running!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
-
+// Route pour ajouter un utilisateur
 app.post("/users", (req, res) => {
   const { first_name, last_name, email, age } = req.body;
 
-  // Validation des données d'entrée
   if (!first_name || !last_name || !email || !age) {
     return res.status(400).json({ message: "Tous les champs sont requis." });
   }
 
-  // Préparer l'insertion dans la base de données
   const stmt = db.prepare(
     "INSERT INTO users (first_name, last_name, email, age) VALUES (?, ?, ?, ?)"
   );
 
-  // Exécuter la requête SQL pour insérer un nouvel utilisateur
   stmt.run(first_name, last_name, email, age, function (err) {
     if (err) {
       return res.status(500).json({ message: "Erreur serveur", error: err });
     }
-    // Retourner une réponse avec l'ID du nouvel utilisateur
     res
       .status(201)
       .json({ id: this.lastID, first_name, last_name, email, age });
   });
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
